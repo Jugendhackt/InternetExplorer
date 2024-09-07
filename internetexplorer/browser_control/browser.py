e6fc84bf401f73ef87ef0109cbfd08b0b4ab517e
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-09-07 14:21:15 +0200

    fix(browser): linux (nixos)

--- internetexplorer/browser_control/browser.py
+++ internetexplorer/browser_control/browser.py
@@ -3,27 +3,31 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from platform import system
 
 
 class browser:
     def __init__(self) -> None:
-        self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        if system() == "Linux":
+            self.browser = webdriver.Chrome()
+        else:
+            self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
     def load_website(self, url: str) -> str:
         self.browser.get(url)
         return str(self.browser.page_source)
-    
+
     def click_element(self, x_path: str) -> bool:
         try: self.browser.find_element(By.XPATH, x_path).click(); return True
         except: return False
-    
+
     def type_text(self, x_path: str, text: str, submit: bool) -> bool:
-        try: 
-            input_element = self.browser.find_element(By.XPATH, x_path); input_element.send_keys(text); 
+        try:
+            input_element = self.browser.find_element(By.XPATH, x_path); input_element.send_keys(text);
             if submit: input_element.send_keys(Keys.ENTER)
             return True
         except: return False
-    
+
     def get_content(self) -> str: return str(self.browser.page_source)
 
     def xpath_exists(self, xpath: str) -> bool:
