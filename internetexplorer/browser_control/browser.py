f80630bad21a401fc43c0cf4f3ce05157a7fd964
Luis Schuimer <85784931+LuisSchuimer@users.noreply.github.com>
2024-09-07 21:58:35 +0200

    Chore: Diable Search Engine Screen

--- internetexplorer/browser_control/browser.py
+++ internetexplorer/browser_control/browser.py
@@ -13,9 +13,11 @@ class Browser:
     def __init__(self) -> None:
         chrome_options = Options()
         chrome_options.add_extension(Path.cwd() / "internetexplorer" / "browser_control" / "i-still-dont-care-about-cookies.crx")
+        chrome_options.add_argument("--disable-search-engine-choice-screen")
 
         if system() == "Linux": self.browser = webdriver.Chrome(options=chrome_options)
         else: self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
 
         self.html = ""
         self.selected_xpath = ""
