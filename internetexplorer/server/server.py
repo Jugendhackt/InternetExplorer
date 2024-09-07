8856927b692508a4ba31f26fe448b8279a3f06a0
Dorothea <Dorothea.Ganswindt@t-online.de>
2024-09-07 22:30:55 +0200

    feat: more server actions

--- internetexplorer/server/server.py
+++ internetexplorer/server/server.py
@@ -1,10 +1,14 @@
 import asyncio
+from dataclasses import dataclass
 import websockets
 import json 
 file = open("config.json")
 configFileContent = json.loads(file.read())
 
-async def handle_client(websocket, path):
+websocket = None
+
+async def handle_client(socket, path):
+    websocket = socket
     print(f"New client connected to path {path}")
 
     # TODO send the state of the settings here
@@ -22,10 +26,14 @@ async def handle_client(websocket, path):
                 setting_id = parsed_message["setting_id"]
                 new_state = parsed_message["new_state"]
                 print(f"Changing setting {setting_id} to {new_state}")
-            
-            # Sende eine Antwort zurück an den Client
-            #response = f"Server hat deine Nachricht '{message}' erhalten."
-            #await websocket.send(response)
+                # TODO implement
+            elif action == "kill":
+                print("Kill switch pressed!")
+                # TODO implement
+            elif action == "submit_prompt":
+                prompt = parsed_message["prompt"]
+                # TODO implement
+
     except websockets.ConnectionClosedOK:
         print("Connection closed.")
 
@@ -33,9 +41,53 @@ async def main():
     async with websockets.serve(handle_client, configFileContent["server"]["address"], configFileContent["server"]["port"]):
         await asyncio.Future()  # run forever       
 
-# Call this message to start the server thread
+async def add():
+    await websocket.send
+
+def send_voice_input(input):
+    send({
+        "action": "input",
+        "inputType": "voice",
+        "inputText": input
+    })
+
+@dataclass
+class BrowseAction:
+    type: str # the function name (e.g. open_website)
+    action_data: dict[str, str] # map of properties for debugging
+    result: str #either "success" or "failure", currently unused
+
+# takes a list of entries that will be grouped
+def send_browse_action_entry(actions):
+    list = []
+
+    for action in actions:
+        list.append({
+            "type": action.type,
+            "actionData": action.action_data,
+            "result": action.result
+        })
+
+    send({
+        "action": "action_entry",
+        "actions": list
+    })
+
+def send(object):
+    string = json.dump(object)
+    if websocket == None:
+        print("Cannot send, the websocket isn't initialized yet!")
+        return
+    asyncio.run(send_async(string))
+
+async def send_async(string):
+    await websocket.send(string)
+
+# Call this function to start the server thread
 def run():
     asyncio.run(main())
 
 if __name__ == "__main__":
     run()
+
+
