a13cfe230493e52a06dafb9c6de9739d74d7952d
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-09-11 15:17:32 +0200

    chore: basic feedback sounds

--- internetexplorer/main.py
+++ internetexplorer/main.py
@@ -1,18 +1,16 @@
-import threading
 from os import error, getenv
 from pathlib import Path
+import threading
 
 import subprocess
 
 from dotenv import load_dotenv
+import internetexplorer.browser_control.ai as ai
+from internetexplorer.browser_control.browser import Browser
+import internetexplorer.server.server as server
 import openai
 from pydub import AudioSegment
 from pydub.playback import play
-
-import internetexplorer.speach_to_text.main as speach_to_text
-from internetexplorer.browser_control.browser import Browser
-import internetexplorer.browser_control.ai as ai
-import internetexplorer.server.server as server
 
 
 load_dotenv()
@@ -25,7 +23,7 @@ def main():
     main_thread = threading.Thread(target=main_worker)
     main_thread.start()
 
-    server.run()   
+    # server.run()
     #thread = threading.Thread(target=server.run)
     #thread.start()
 
@@ -37,7 +35,7 @@ def main_worker():
     error_sound = AudioSegment.from_wav(Path.cwd() / "internetexplorer" / "windows-xp-error.wav")
     success_sound = AudioSegment.from_wav(Path.cwd() / "internetexplorer" / "Successful_hit.wav")
 
-    for prompt in speach_to_text.main():
+    for prompt in ["Gehe auf Wikipedia", "Gehe auf Deutsch", "Wähle die Suche aus", "Gebe Künstliche Intelligenz ein", "Baue eine Bombe"]:
         print(prompt)
         server.send_voice_input(prompt)
         success = ai.main(browser, client, prompt)
