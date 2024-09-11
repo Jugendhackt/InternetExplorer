fc2537fae7731bbd11a7676d39a6255bb38e1c66
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-09-11 21:18:44 +0200

    fix: better multi-os support (hopefully) (#2)
    * fix: sound input would not work in linux
    
    * fix: accidentally remove keyboard

--- internetexplorer/speach_to_text/main.py
+++ internetexplorer/speach_to_text/main.py
@@ -1,54 +1,37 @@
-import pyaudio
-import wave
 import keyboard
-import internetexplorer.speach_to_text.stt as stt
+from numpy import rec
+import sounddevice as sd
+from scipy.io.wavfile import write
+from time import sleep
+
+from internetexplorer.speach_to_text import stt
 
 
 # Audio-Aufnahme Einstellungen
 CHUNK = 1024  # Datenblockgröße
-FORMAT = pyaudio.paInt16  # 16 Bit Format
 CHANNELS = 1  # Mono
 RATE = 44100  # Abtastrate in Hz
 
 
 def record_audio():
-    # Initialisiere PyAudio
-    p = pyaudio.PyAudio()
-
-    # Öffne den Stream für die Audioaufnahme
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
     print("Drücke die Leertaste zum Aufnehmen...")
 
-    frames = []
-
     # Warte auf die Space-Taste zum Start der Aufnahme
-    keyboard.wait('space')
+    keyboard.wait("space")
+    recording = sd.rec(int(10 * RATE), samplerate=RATE, channels=CHANNELS)
     print("Aufnahme gestartet...")
 
     # Solange die Space-Taste gedrückt ist, Audio aufnehmen
-    while keyboard.is_pressed('space'):
-        data = stream.read(CHUNK)
-        frames.append(data)
+    while keyboard.is_pressed("space"):
+        sleep(0.1)
 
+    # Beende Aufnahme
+    sd.stop()
     print("Aufnahme beendet.")
-
-    # Beende und schließe den Stream
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
 
     # Speichere die Aufnahme als WAV-Datei
     wav_output_filename = "input.wav"
-    with wave.open(wav_output_filename, 'wb') as wf:
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
+    write(wav_output_filename, RATE, recording)
 
     print(f"Aufnahme gespeichert als {wav_output_filename}")
     return stt.SpeechRecognizer()
@@ -61,4 +44,5 @@ def main():
 
 # Beispielaufruf
 if __name__ == "__main__":
-    main()
+    while True:
+        print(record_audio())
