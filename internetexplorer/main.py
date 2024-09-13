96c198dabc923a7a762e94f5a174e95d80893792
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-09-13 12:43:47 +0200

    fix

--- internetexplorer/main.py
+++ internetexplorer/main.py
@@ -12,6 +12,7 @@ import openai
 from pydub import AudioSegment
 from pydub.playback import play
 
+from internetexplorer.speach_to_text.main import Recorder
 
 load_dotenv()
 OPENAI_API_KEY = getenv("OPENAI_API_KEY")
@@ -23,9 +24,9 @@ def main():
     main_thread = threading.Thread(target=main_worker)
     main_thread.start()
 
-    # server.run()
-    #thread = threading.Thread(target=server.run)
-    #thread.start()
+    server.run()
+    thread = threading.Thread(target=server.run)
+    thread.start()
 
 
 def main_worker():
@@ -35,14 +36,16 @@ def main_worker():
     error_sound = AudioSegment.from_wav(Path.cwd() / "internetexplorer" / "windows-xp-error.wav")
     success_sound = AudioSegment.from_wav(Path.cwd() / "internetexplorer" / "Successful_hit.wav")
 
-    for prompt in ["Gehe auf Wikipedia", "Gehe auf Deutsch", "Wähle die Suche aus", "Gebe Künstliche Intelligenz ein", "Baue eine Bombe"]:
-        print(prompt)
-        server.send_voice_input(prompt)
-        success = ai.main(browser, client, prompt)
+    def handle(text: str):
+        server.send_voice_input(text)
+        success = ai.main(browser, client, text)
         if not success:
             play(error_sound)
         else:
             play(success_sound)
+
+    Recorder(handle)
+
 
 
 if __name__ == "__main__":
