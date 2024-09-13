bd27ada522a187bf97e1d5f06e5565cceed8fcbb
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-09-13 12:39:50 +0200

    ci: nix project flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in
+  {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      packages = with pkgs; [
+        python311
+        python311Packages.selenium
+        python311Packages.openai
+        python311Packages.python-dotenv
+        python311Packages.python-socketio
+        python311Packages.flask
+        python311Packages.speechrecognition
+        python311Packages.websockets
+        python311Packages.pydub
+        python311Packages.scipy
+        python311Packages.webdriver-manager
+        python311Packages.beautifulsoup4
+        python311Packages.pynput
+        python311Packages.pyaudio
+      ];
+    };
+  };
+}
