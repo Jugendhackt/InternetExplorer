9cc63de4ed81d32b9aa6fcf68c84c5b1ebe63dd9
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-09-15 00:47:05 +0200

    fix(flake): chromedriver missing

--- flake.nix
+++ flake.nix
@@ -5,13 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-  let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
-  in
-  {
+  in {
     devShells."x86_64-linux".default = pkgs.mkShell {
       packages = with pkgs; [
+        chromedriver
+
         python311
         python311Packages.selenium
         python311Packages.openai
