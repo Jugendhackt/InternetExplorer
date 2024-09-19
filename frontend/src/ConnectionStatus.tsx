886e4ea47dfdb8d1ef8bbd0940bb073ce6ddd5fb
PhilemonDev <125824726+PhilemonDev@users.noreply.github.com>
2024-09-19 22:39:23 +0200

    feat(frontend): add tooltip to connection status

--- frontend/src/ConnectionStatus.tsx
+++ frontend/src/ConnectionStatus.tsx
@@ -1,4 +1,10 @@
-import { ExclamationOctagon, ExclamationTriangle, Hourglass, Wifi, X } from "react-bootstrap-icons";
+import {
+  ExclamationOctagon,
+  ExclamationTriangle,
+  Hourglass,
+  Wifi,
+  X,
+} from "react-bootstrap-icons";
 import { ReadyState } from "react-use-websocket";
 
 interface Props {
@@ -7,15 +13,22 @@ interface Props {
 
 const ConnectionStatus = ({ readyState }: Props) => {
   if (readyState == ReadyState.OPEN) {
-    return <Wifi className="text-success" />;
+    return <Wifi className="text-success" title="Connected to server." />;
   } else if (readyState == ReadyState.CLOSED) {
-    return <X className="text-danger" />;
+    return <X className="text-danger" title="Failed to connect." />;
   } else if (readyState == ReadyState.CLOSING) {
-    return <ExclamationTriangle className="text-warning" />
+    return (
+      <ExclamationTriangle
+        className="text-warning"
+        title="Closing connection..."
+      />
+    );
   } else if (readyState == ReadyState.CONNECTING) {
-    return <Hourglass className="text-info" />
+    return <Hourglass className="text-info" title="Attempting to connect..." />;
   } else if (readyState == ReadyState.UNINSTANTIATED) {
-    return <ExclamationOctagon className="text-danger"/>
+    return (
+      <ExclamationOctagon className="text-danger" title="Not instantiated!" />
+    );
   }
   return <span className="text-danger">Failed to load status!</span>;
 };
